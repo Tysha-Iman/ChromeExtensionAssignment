1b9810b9e605bb2d1693ab34a6fc8b58377cacb2
Latysha Mohamed <Tysha@Latyshas-MacBook-Pro.local>
2021-02-24 13:34:11 -0700

    Replace words

--- js/content.js
+++ js/content.js
@@ -11,6 +11,27 @@ $("*").each( function () {
     }
 });
 
+//code from https://9to5google.com/2015/06/14/how-to-make-a-chrome-extensions/ for word replacement
+var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var replacedText = text.
+            replace(/hello/gi, 'Ahoy, matey');
+
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }
+    }
+}
+
 var cursorP = chrome.runtime.getURL("images/cursorpointer.png");
 var cursorH = chrome.runtime.getURL("images/cursorhover.png");
 
