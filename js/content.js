d0521a417f4ba536f93175ab4e9817f425dbb128
Latysha Mohamed <Tysha@Latyshas-MacBook-Pro.local>
2021-02-24 06:10:20 -0700

    Trying my Best

--- js/content.js
+++ js/content.js
@@ -2,9 +2,15 @@ console.log("extension is running!")
 
 var	imgURL = chrome.runtime.getURL("images/old-paper.png");
 var imgURL2 = chrome.runtime.getURL("images/vignette.png");
-
+var cursorP = chrome.runtime.getURL("images/cursorpointer.png");
+var cursorH = chrome.runtime.getURL("images/cursorhover.png");
 
 $("body").css("background-image", "url("+ imgURL +")");
+// $("body").addClass("cursorimg"); 
+
+// $(document).ready(function(){
+//  var cursor = $('.cursorimg');
+// });
 
 $("*").each( function () {
     var $this = $(this);
