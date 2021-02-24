7838e9790b39c526589a2ee2cc87c43613e38c0f
Latysha Mohamed <Tysha@Latyshas-MacBook-Pro.local>
2021-02-24 08:21:42 -0700

    giving up

---
+++ README.md
@@ -0,0 +1,7 @@
+#PIRATE FILTER CHROME EXTENSION 
+
+The purpose of the extension was to make a pirate theme browser experience; but ultimately changed to me trying my best. 
+
+##Inspiration
+*[coffeelings](https://chrome.google.com/webstore/detail/coffeelings/hcbddpppkcnfjifbcfnhmelpemdoepkk)
+*[Cute Cursors](https://chrome.google.com/webstore/detail/cute-cursors-custom-curso/anflghppebdhjipndogapfagemgnlblh)
--- js/content.js
+++ js/content.js
@@ -2,15 +2,7 @@ console.log("extension is running!")
 
 var	imgURL = chrome.runtime.getURL("images/old-paper.png");
 var imgURL2 = chrome.runtime.getURL("images/vignette.png");
-var cursorP = chrome.runtime.getURL("images/cursorpointer.png");
-var cursorH = chrome.runtime.getURL("images/cursorhover.png");
 
-$("body").css("background-image", "url("+ imgURL +")");
-// $("body").addClass("cursorimg"); 
-
-// $(document).ready(function(){
-//  var cursor = $('.cursorimg');
-// });
 
 $("*").each( function () {
     var $this = $(this);
@@ -18,5 +10,29 @@ $("*").each( function () {
         $this.css({ "font-size": "20px" });   
     }
 });
+
+var cursorP = chrome.runtime.getURL("images/cursorpointer.png");
+var cursorH = chrome.runtime.getURL("images/cursorhover.png");
+
+$document.css({
+    'position': 'absolute', 
+    'top':'5px',
+    'left':'5px',
+    'cursor': cursorH
+});
+
+// var Normalwords = new array ["hello", "yes", "my", "?", "you"];
+// var PirateSlang = new array ["Ahoy, matey", "aye", "me", "Savvy?", "ye"];
+// var ourNewString = Normalwords
+//     .replace("hello", "Ahoy")
+//     .replace("yes", "aye")
+//     .replace("my", "me")
+//     .replace("?", "Savvy?")
+//     .replace("you", "ye");
+
+// console.log(ourNewString);
+
+
+// $("body").css("cursor" : "url("+ cursorP +")");
 
 
